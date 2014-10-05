1b69df90b956892998dce1a23072d50d9b8424a6
Trevor Owens <trevorkowens@ufl.edu>
2014-10-05 15:48:58 -0400

    Backend and Frotend all work! Yay!

--- mean-master/app/models/article.server.model.js
+++ mean-master/app/models/article.server.model.js
@@ -9,26 +9,56 @@ var mongoose = require('mongoose'),
 /**
  * Article Schema
  */
-var ArticleSchema = new Schema({
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	title: {
-		type: String,
-		default: '',
-		trim: true,
-		required: 'Title cannot be blank'
-	},
-	content: {
-		type: String,
-		default: '',
-		trim: true
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+var ArticleSchema = new Schema({}); // temporary
+var EventSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        default: '',
+        required: 'Must have a title'
+    },
+    description: {
+        type: String,
+        default: ''
+        // not required
+    },
+    sponsor: {
+        type: String,
+        default: '',
+        required: 'Must have a sponsor'
+    },
+    contactEmail: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Must have a contact Email'
+    },
+    contactPhone: {
+        type: String,
+        default: ''
+        // not required
+    },
+    room: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Must be a valid number'
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: 'Must be a valid date'
+    },
+    time_period: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Must be specified to a period on the UF schedule'
+    }
 });
 
+mongoose.model('Event', EventSchema);
 mongoose.model('Article', ArticleSchema);
