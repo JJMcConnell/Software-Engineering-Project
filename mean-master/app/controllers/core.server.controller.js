ab09b6ccce508e21fdac6131da9a0beee20f2f27
Trevor Owens <trevorkowens@ufl.edu>
2014-10-05 16:29:21 -0400

    New Commit

--- mean-master/app/controllers/core.server.controller.js
+++ mean-master/app/controllers/core.server.controller.js
@@ -24,6 +24,28 @@ exports.getAvailableRooms = function (req, res) {
     
 }
 
+//http://localhost:3000/test takes you to this method
+//add http://localhost:3000/test?roomnumber= asdfasdf
+exports.test = function (req, res) {
+    //Assume req.body contains date which is a date object, room number which is a string
+
+    //get is req.query
+    //var date = req.query.date;
+
+
+
+    var roomNumber = req.query.roomNumber;
+    var date = new Date(2014, 10, 10);
+    var roomNumber = "120";
+    //Takes a date, and a roomNumber, and returns an array of which periods are/aren't available
+
+    console.log(roomNumber);
+    //res.json(the array) or something returns result
+
+
+    //THIS IS IMPORTANT because
+    res.redirect('/');
+}
 
 exports.addevent = function (req, res) {
     //get is req.query
@@ -41,9 +63,8 @@ exports.addevent = function (req, res) {
         time_period: '5'
     });
 
-    testEvent.save(function (err, thor) {
+    testEvent.save(function (err) {
         if (err) return console.error(err);
-        console.dir(thor);
     });
     
     Event.find({ "title": 'Trevor' }, function (err, doc) {
--- mean-master/app/routes/core.server.routes.js
+++ mean-master/app/routes/core.server.routes.js
@@ -5,4 +5,6 @@ module.exports = function(app) {
 	var core = require('../../app/controllers/core');
 	app.route('/').get(core.index);
 	app.route('/post').get(core.addevent);
+	//Makes http://localhost:3000/test go to method core.test as defined in controllers/core.server.controller
+	app.route('/test').get(core.test);
 };
