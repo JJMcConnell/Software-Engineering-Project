517971a5a2d6a3093a30132e12016b5ce57a6a37
dannysepler <dsepler@ufl.edu>
2014-10-09 00:19:34 -0400

    further work on individualized room html pages

--- mean-master/app/routes/core.server.routes.js
+++ mean-master/app/routes/core.server.routes.js
@@ -7,6 +7,16 @@ module.exports = function(app) {
     app.route('/requestevent').get(core.addevent);
 	//Makes http://localhost:3000/test go to method core.test as defined in controllers/core.server.controller
     app.route('/test').get(core.test);
+    
     app.route('/roomlookup/:tagId').get(core.ThisRoom);
+    	// this doesn't work, just prints blank html page
+    	// no idea why
+    	// 		~~ Danny
+
     app.route('/roomlookups').get(core.ThisRoom);
+    	// on the other hand, this one works just fine
+    	// i feel like it shouldn't matter, considering
+    	// they're just rendering the same function and
+    	// the same page
+    	// 		~~ Danny
 };
