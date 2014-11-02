ace3f5a715c6192d30b96133d618b2ebab9b8fc3
ddachille <ddachille@ufl.edu>
2014-11-02 01:06:00 -0500

    just fixed so the tests could run. The last test that can't run is fine, I didn't finish it but I wanted to commit since I will be gone all day tomorrow.

--- mean-master/app/tests/event.server.model.test.js
+++ mean-master/app/tests/event.server.model.test.js
@@ -103,15 +103,15 @@ describe('Event Model Unit Tests:', function() {
 	    });	
 
 	    it('should find events by day and room number', function (done) {
-	        Event.find({ 'room': '121', 'date': '10-12-2014' }, 'time_period', function (err, events) {
+	        Event.find({ 'room': the_room, 'day': the_day, 'month': the_month, 'year': '2014'}, 'time_period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length > 0)
 	                done();
 	        });
 	    });
 
-	    it('should find events by day and period', function (done) {
-	        Event.find({ 'room': '121', 'time_period': '5' }, 'time_period', function (err, events) {
+	    it('should find events by room and period', function (done) {
+	        Event.find({ 'room': the_room, 'period': the_period }, 'time_period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length > 0)
 	                done();
@@ -119,7 +119,7 @@ describe('Event Model Unit Tests:', function() {
 	    });
 
 	    it('should find events by period only', function (done) {
-	        Event.find({ 'time_period': '5' }, 'time_period', function (err, events) {
+	        Event.find({ 'period': the_period }, 'time_period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length > 0)
 	                done();
@@ -127,24 +127,38 @@ describe('Event Model Unit Tests:', function() {
 	    });
 
 	    it('should find events by room number', function (done) {
-	        Event.find({ 'room': '121' }, 'time_period', function (err, events) {
+	        Event.find({ 'room': '121' }, 'period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length > 0)
 	                done();
 	        });
 	    });	
 	    it('should find events by day', function (done) {
-	        Event.find({ 'date': '10-12-2014' }, 'time_period', function (err, events) {
+	        Event.find({ 'day': the_day, 'month': the_month, 'year': '2014' }, 'period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length > 0)
 	                done();
 	        });
-	    });		
+	    });	
+	    it('should find events by month', function (done) {
+            Event.find({'month': the_month, 'year': '2014' }, 'period', function (err, events) {
+                should.not.exist(err);
+                if (events.length > 0)
+                    done();
+            });
+        });	
+        it('should find events by year', function (done) {
+            Event.find({ 'month': the_month, 'year': '2014' }, 'period', function (err, events) {
+                should.not.exist(err);
+                if (events.length > 0)
+                    done();
+            });
+        });
 	});
 
 	describe('Where no events should be found', function() {
    		it('should not find events in date w/ no events', function (done) {
-	        Event.find({ 'date': '10-12-2015' }, 'time_period', function (err, events) {
+	        Event.find({ 'day': the_day, 'month': the_month, 'year': '2015' }, 'time_period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length == 0)
 	                done();
@@ -161,7 +175,7 @@ describe('Event Model Unit Tests:', function() {
 	    });	
 
 	    it('should not find events at room with no period', function (done) {
-	        Event.find({ 'room': '123', 'time_period': '5' }, 'time_period', function (err, events) {
+	        Event.find({ 'room': '120', 'period': '54' }, 'time_period', function (err, events) {
 	            should.not.exist(err);
 	            if (events.length == 0)
 	                done();
@@ -170,156 +184,40 @@ describe('Event Model Unit Tests:', function() {
 	});
 
 
-	afterEach(function(done) {
-		done();
+
+	//admin approved test
+	//admin rejected test
+	
+	describe('Admin Functions', function() {
+			it("should approve the request", function (done) {
+		        	var id = myEvent._id;
+
+    				Event.findByIdAndUpdate( id, 
+        				{ 'viewed': true, 'approved': true }, 
+        					function(err, events) {}
+    				);
+
+    				Event.findById( id, 'period', function (err, event) {
+	            		should.not.exist(err);
+	            		if (event.approved == true)
+	                		done();
+	        		});
+		        
+			});
+
+
+		   /* it('should reject the request', function (done) {
+	       	    	return myEvent.save(function (err) {
+		            	should.not.exist(err);
+		           	 done();
+		        	});
+		    });*/
 	});
 
-/*
-describe('Event Model Unit Tests:', function () {
-    beforeEach(function (done) {
-        // create sample event
-        myEvent = new Event({
-            title: 'TestTitle',
-            sponsor: 'TestSponsor',
-            contactEmail: 'trevorkowens@gmail.com',
-            room: '121',
-            year: '2014',
-            month: '12',
-            day: '10',
-            period: '5'
-        });
-        done();
-    });
-
-    var assert = require('assert'),
-    http = require('http');
-
-    describe('/', function () {
-        it('should connect to localhost', function (done) {
-            http.get('http://localhost:3000', function (res) {
-                done();
-            });
-        });
-
-    });
-
-
-    describe('Method Save', function () {
-        it("should connect to database", function (done) {
-            var db = mongoose.connect('mongodb://master:master@ds039850.mongolab.com:39850/projectdb', function (err) {
-                if (err)
-                    mongoose.connection.on('error', function (err) {
-                        should.not.exist(err);
-                    });
-                done();
-            });
-        });
-
-
-        it('should be able to save events in database', function (done) {
-            return myEvent.save(function (err) {
-                console.log(err);
-                should.not.exist(err);
-                done();
-            });
-        });
-    });
-    describe('Where events should be found', function () {
-
-        it('should find events given no parameters', function (done) {
-            Event.find({}, function (err, events) {
-                should.not.exist(err);
-                console.log(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-
-        it('should find events by day and room number', function (done) {
-            Event.find({ 'room': '121', 'year': '2014', 'day':'10', month:'12' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-
-        it('should find events by day and period', function (done) {
-            Event.find({ 'room': '121', 'period': '5' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-
-        it('should find events by period only', function (done) {
-            Event.find({ 'period': '5' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-
-        it('should find events by room number', function (done) {
-            Event.find({ 'room': '121' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-        it('should find events by day', function (done) {
-            Event.find({ 'day': '10', 'month':'12', 'year': '2014' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-        it('should find events by month', function (done) {
-            Event.find({'month': '12', 'year': '2014' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-
-        it('should find events by year', function (done) {
-            Event.find({ 'month': '12', 'year': '2014' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length > 0)
-                    done();
-            });
-        });
-    });
-
-    describe('Where no events should be found', function () {
-        it('should not find events in date w/ no events', function (done) {
-            Event.find({ 'day': '40', 'month':'12', 'year':'2014' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length == 0)
-                    done();
-            });
-        });
-
-
-        it('should not find events in room w/ no events', function (done) {
-            Event.find({ 'room': '123' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length == 0)
-                    done();
-            });
-        });
-
-        it('should not find events at room with no period', function (done) {
-            Event.find({ 'room': '123', 'period': '5' }, 'period', function (err, events) {
-                should.not.exist(err);
-                if (events.length == 0)
-                    done();
-            });
-        });
-    });
-    */
 
 
     afterEach(function (done) {
+        /*
         Event.remove({ _id: myEvent.id }, function (err) {
             if (!err) {
                 done();
@@ -327,7 +225,7 @@ describe('Event Model Unit Tests:', function () {
             else {
                 console.log('error with afterEach');
             }
-        });
+        });*/
         done();
     });
 });
--- mean-master/app/tests/mailer.js
+++ mean-master/app/tests/mailer.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var mailer = require("mailer")
 , username = "trevorkowens@gmail.com"
