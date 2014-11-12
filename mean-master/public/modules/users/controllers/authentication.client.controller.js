a1f85a500b6842bb49de8884954bcce5410eaaa6
Trevor Owens <trevorkowens@ufl.edu>
2014-11-12 15:23:49 -0500

    Made the requested events show date submitted show correctly and in the correct order.

--- mean-master/public/modules/users/controllers/authentication.client.controller.js
+++ mean-master/public/modules/users/controllers/authentication.client.controller.js
@@ -9,6 +9,14 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		    if (!$scope.authentication.user) $location.path('/');
 		}
 
+
+
+		$scope.parseWeirdDate = function (weirdDate) {
+		    var newDate = weirdDate.substring(11, 19) + ', ' + weirdDate.substring(5,7) +"/"+weirdDate.substring(8,10)+"/"+ weirdDate.substring(0, 4);
+		    return newDate;
+		}
+
+
 		$scope.events = [];
 		$scope.fetchRequests = function () {
 		    $http.get('/fetchRequests', $scope.credentials).success(function (response) {
