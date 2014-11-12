f2a2f2c82f0a3a7adf59ae99ea7d6ba7831e837e
yanayeg <yanayeg@ufl.edu>
2014-11-12 17:21:22 -0500

    info modal for admin page

---
+++ mean-master/public/modules/users/controllers/ModalInfoInstanceCtrl.js
@@ -0,0 +1,18 @@
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+angular.module('users').controller('ModalInfoInstanceCtrl', function ($scope, $modalInstance, items, $http, $location) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
