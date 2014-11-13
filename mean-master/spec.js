43f09ad4ce8b08b3806cd71aafb6d85dbfb85dcf
Joseph McConnell <jjmdevelops@gmail.com>
2014-11-12 19:57:19 -0500

    Some really basic test stuff

--- mean-master/spec.js
+++ mean-master/spec.js
@@ -1,5 +1,27 @@
 describe('angularjs homepage', function() {
 
+//all of the room buttons
+	var room101 = element(by.id('101'));
+	var room106 = element(by.id('106'));
+	var room120 = element(by.id('120'));
+	var room121 = element(by.id('121'));
+	var room142 = element(by.id('142'));
+	var room143 = element(by.id('143'));
+	var room144 = element(by.id('144'));
+	var room145 = element(by.id('145'));
+	var room146 = element(by.id('146'));
+	var room147 = element(by.id('147'));
+	var room232 = element(by.id('232'));
+	var room233 = element(by.id('233'));
+
+	var cancelButton = element(by.buttonText("Cancel"));
+
+//Array containing all of the room options and random number within array
+	var roomButtons = [room101, room106, room120, room121, room142, room143, room144, room145, room146, room147, room232, room233];
+	var randomnumber = Math.floor(Math.random() * (11 - 0 + 1)) + 0;
+
+
+	var modal = element(by.className('modal-header'));
 
 	beforeEach(function() {
 		browser.get('http://localhost:3000')
@@ -9,6 +31,25 @@ describe('angularjs homepage', function() {
 		expect(browser.getTitle()).toEqual('The University of Florida Department of Music Presents: Room Scheduler')
 	});
 
-	
+	it('should open up the modal', function() {
+
+		roomButtons[randomnumber].click();
+
+		expect(modal.isPresent()).toBeTruthy();
+
+	});
+
+	 it('should test that all of the buttons work', function() {
+
+	 	for (i = 0; i < roomButtons.length; i++) {
+	 		roomButtons[i].click();
+	 		cancelButton.click();
+	 	}
+	 	
+	 	expect(modal.isPresent()).toBeFalsy();
+
+	});
+
+
 
 });
