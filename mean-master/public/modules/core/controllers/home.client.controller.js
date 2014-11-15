f6e8abda4ba8b1365a22180d2061400fde99ac8a
ddachille <ddachille@ufl.edu>
2014-11-15 14:58:25 -0500

    Trying to generate Admin

--- mean-master/public/modules/core/controllers/home.client.controller.js
+++ mean-master/public/modules/core/controllers/home.client.controller.js
@@ -3,5 +3,11 @@
 
 angular.module('core').controller('HomeController', ['$scope',
 	function($scope) {
-	    
+	      $scope.generateAdmin = function () {
+	     $http.post('/generateAdmin').success(function (response) {
+      		}).error(function (response) {
+          		$scope.error = "Error creating admin";
+          		console.log($scope.error);
+      		});
+	    };
 	}]);
