4fa338bc0019323348f5cff784457a8ea7d78553
Clarence <clarence.hwang@gmail.com>
2014-12-03 22:18:06 -0500

    Changed calendar to home page, navbar

--- mean-master/public/modules/core/config/core.client.routes.js
+++ mean-master/public/modules/core/config/core.client.routes.js
@@ -13,18 +13,19 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			});*/
 	    // Home state routing
 	    $stateProvider.
+        /*state('home', {
+            url: '/',
+            templateUrl: 'modules/core/views/home.client.view.html'
+        }).        */
 		state('calendar', {
-		    url: '/calendar',
+		    url: '/',
 		    templateUrl: 'modules/core/views/calendar.client.view.html'
 		}).
 		state('uncalendar', {
 			url: '/uncalendar',
 			templateUrl: 'modules/core/views/uncalendar.client.view.html'
 		}).
-        state('home', {
-            url: '/',
-            templateUrl: 'modules/core/views/home.client.view.html'
-        }).
+
         state('room calendar', {
             url: '/calendarForRoom',
             templateUrl: 'modules/core/views/roomCalendar.client.view.html'
